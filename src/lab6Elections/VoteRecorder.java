b831078e14d4a007bd29e270efc92353889f5216
Ylvakiller <rem.geuze@gmail.com>
2014-10-10 13:55:13 +0200

    added javadoc, deleted voteHolder class

--- src/lab6Elections/VoteRecorder.java
+++ src/lab6Elections/VoteRecorder.java
@@ -1,12 +1,31 @@
 package lab6Elections;
 /**
- * @author Ylva
  * Holds the names for the candidates for both president and vice-president
  * Records the total amount of votes
  * Allows the names for candidates to be changed, but only when the number of votes is 0 for all candidates
  * Allows the votes to be reset. 
  * Can return the standings for the election
+ * 
+ * @author Ylva
  */
 public class VoteRecorder {
+	/**
+	 * Name of the first candidate for president
+	 */
+	public static String nameCandidatePresident1;
+	/**
+	 * Name of the second candidate for President
+	 */
+	public static String nameCandidatePresident2;
+	/**
+	 * Name of the first Vice President candidate
+	 */
+	public static String nameCandidateVicePresident1;
+	/**
+	 * Name of the second Vice President Candidate
+	 */
+	public static String nameCandidateVicePresident2;
+	
+	
 
 }
