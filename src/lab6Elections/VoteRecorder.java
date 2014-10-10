cc5f9f4e03ae57cf0c26d8ba646329b7c1fb11b3
Ylvakiller <rem.geuze@gmail.com>
2014-10-10 14:03:16 +0200

    Added static variables for number of votes
    Included javadoc for those static variables

--- src/lab6Elections/VoteRecorder.java
+++ src/lab6Elections/VoteRecorder.java
@@ -6,26 +6,46 @@ package lab6Elections;
  * Allows the votes to be reset. 
  * Can return the standings for the election
  * 
- * Records the vote for a specific object
+ * Records the vote for a specific voter
  * @author Ylva
  */
 public class VoteRecorder {
 	/**
-	 * Name of the first candidate for president
+	 * Name of the first candidate for president.
 	 */
 	public static String nameCandidatePresident1;
 	/**
-	 * Name of the second candidate for President
+	 * Name of the second candidate for President.
 	 */
 	public static String nameCandidatePresident2;
 	/**
-	 * Name of the first Vice President candidate
+	 * Name of the first Vice President candidate.
 	 */
 	public static String nameCandidateVicePresident1;
 	/**
-	 * Name of the second Vice President Candidate
+	 * Name of the second Vice President Candidate.
 	 */
 	public static String nameCandidateVicePresident2;
+	
+	/**
+	 * Total amount of votes that have been cast for the first candidate for president.
+	 */
+	public static int votesCandidatePresident1;
+	
+	/**
+	 * Total amount of votes that have been cast for the second candidate for president.
+	 */
+	public static int votesCandidatePresident2;
+	
+	/**
+	 * Total amount of votes that have been cast for the first candidate for vice president.
+	 */
+	public static int votesCandidateVicePresident1;
+	
+	/**
+	 * Total amount of votes that have been cast for the second candidate for vice president.
+	 */
+	public static int votesCandidateVicePresident2;
 	
 	
 	
