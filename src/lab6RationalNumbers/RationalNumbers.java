fee0603de8a4a2e2ccf36bf2e4a59a541416279e
Ylvakiller <rem.geuze@gmail.com>
2014-10-12 17:54:43 +0200

    Started with the rationNumbers class

--- src/lab6RationalNumbers/RationalNumbers.java
+++ src/lab6RationalNumbers/RationalNumbers.java
@@ -2,4 +2,20 @@ package lab6RationalNumbers;
 
 public class RationalNumbers {
 
+	/**
+	 * The numerator is the number before the / sign
+	 */
+	private int numerator;
+	
+	/**
+	 * The denominator is the number after the / sign
+	 */
+	private int denominator;
+	
+	/**
+	 * Default constructor, sets the rational number to 0/1
+	 */
+	public RationalNumbers(){
+		
+	}
 }
