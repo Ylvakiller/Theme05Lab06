642b14b2d9e91652101ee875c41a7688d5697259
Ylvakiller <rem.geuze@gmail.com>
2014-10-12 18:07:47 +0200

    Added the getGCD method

--- src/lab6RationalNumbers/RationalNumbers.java
+++ src/lab6RationalNumbers/RationalNumbers.java
@@ -1,5 +1,7 @@
 package lab6RationalNumbers;
 
+import java.math.BigInteger;
+
 public class RationalNumbers {
 
 	/**
@@ -16,6 +18,21 @@ public class RationalNumbers {
 	 * Default constructor, sets the rational number to 0/1
 	 */
 	public RationalNumbers(){
-		
+		numerator =0;
+		denominator = 0;
+	}
+	
+	/**
+	 * Calculates the greatest common divisor of the inputs.
+	 * Note that the inputs should be non zero integers
+	 * @param numerator Used in the calculations as a numerator
+	 * @param denominator Used in the calculations as a denominator
+	 * @return an int that is the greatest common divisor
+	 */
+	public int getGCD(int numerator, int denominator){
+		BigInteger tempNumerator =BigInteger.valueOf(numerator);
+		BigInteger tempDenominator =BigInteger.valueOf(denominator);
+		BigInteger tempGCD = tempNumerator.gcd(tempDenominator);
+		return tempGCD.intValue();
 	}
 }
